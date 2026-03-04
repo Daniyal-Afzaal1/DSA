cdd53322516f869699877b2b7420b19ec907b3cf
Daniyal-Afzaal1 <daniyalafzaal@yahoo.com>
2026-03-04 17:09:16 +0500

    Added 9 and 10 Pattern

---
+++ Patterns/09_Diamond_Star_Pattern.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int limit = 6;
+    int counter = 5;
+    int starloop = 0;
+    for (int k = 0; k < 5; k++)
+    {
+        for (int i = 1; i <= limit; i++)
+        {
+            bool visited = false;
+            starloop = limit - counter;
+            if (i <= counter)
+            {
+                cout << " ";
+            }
+            else
+            {
+                for (int j = starloop; j > 0; j--)
+                {
+                    cout << "*";
+                    visited = true;
+                }
+            }
+            if (visited)
+            {
+                i = limit + 1;
+            }
+
+            if (i == limit || i == limit + 1)
+            {
+                limit++;
+                counter--;
+                starloop = 0;
+                cout << endl;
+            }
+        }
+    }
+    limit = 10;
+    counter = 1;
+    starloop = 0;
+    for (int k = 0; k < 5; k++)
+    {
+        for (int i = 1; i <= limit; i++)
+        {
+            bool visited = false;
+            starloop = limit - counter;
+            if (i <= counter)
+            {
+                cout << " ";
+            }
+            else
+            {
+                for (int j = starloop; j > 0; j--)
+                {
+                    cout << "*";
+                    visited = true;
+                }
+            }
+            if (visited)
+            {
+                i = limit + 1;
+            }
+
+            if (i == limit || i == limit + 1)
+            {
+                limit--;
+                counter++;
+                starloop = 0;
+                cout << endl;
+            }
+        }
+    }
+}
